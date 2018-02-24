df75969bde5975934cbc89eaaa6db8f86cfc261b
fogelkuba <fogelkuba@gmail.com>
2018-02-24 18:49:28 +0100

    1

--- src/Person.js
+++ src/Person.js
@@ -2,4 +2,11 @@ class Person {
     constructor(name){
         this.name = name;
     }
+    greet() {
+        return `Welcome ${this.name}!`;
+    }
 }
+
+
+
+console.log( new Person('John').greet() );
